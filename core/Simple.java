7a69eec05cb9bdcf9cae6881282253cea928eb99
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-08-05 23:21:26 +0700

    variables

---
+++ core/Simple.java
@@ -0,0 +1,23 @@
+public class Simple {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = a + b;
+        float f=a;
+        float g=10.5f;
+        int h = (int)g;
+        int i = 130;
+        byte j = (byte)i;
+        byte k = 10;
+        byte l = 20;
+        byte m = (byte)(k + l);
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(f);
+
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(j);
+        System.out.println(m);
+    }
+}

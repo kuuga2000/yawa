2c250aef861589b295d3d8957ef1621b5c6610a8
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-08-06 00:22:36 +0700

    constructor

---
+++ core/Car.java
@@ -0,0 +1,13 @@
+//Java Program to create and call a default constructor
+class Car {
+    // creating a default constructor
+    Car() {
+        System.out.println("Car is created");
+    }
+
+    // main method
+    public static void main(String args[]) {
+        // calling a default constructor
+        Car b = new Car();
+    }
+}
---
+++ core/Plane.java
@@ -0,0 +1,27 @@
+class Plane {
+    int id;
+    String name;
+    int age;
+
+    Plane(int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    Plane(int i, String n, int a) {
+        id = i;
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println(id + " " + name + " " + age);
+    }
+
+    public static void main(String args[]) {
+        Plane p1 = new Plane(800, "Boeing");
+        Plane p2 = new Plane(380, "Airbus",300);
+        p1.display();
+        p2.display();
+    }
+}
